d1d791e3a7c24e14c7f3e365770d6091ec62c0ad
grzegorztomaszewski <aroxd3@gmail.com>
2019-04-15 16:56:49 +0200

    tablice

---
+++ Tablice/script.js
@@ -0,0 +1,37 @@
+/*
+//Tablice
+const tab = []; //pusta tablica
+const tab2 = [1,2,3,4]; //tablica z 4 elementami
+const tab3 = ['Marcin', 'Ania', 'Agnieszka']; //tablica z 3 elementami
+
+const a = "ALA";
+const b = 234;
+const c = "PIES";
+const tab4 = [a,b,c,"KOT"];
+
+console.log(tab4[0]);
+console.log(tab4[1]);
+console.log(tab4[2]);
+console.log(tab4[3]);
+
+const tab5 = ["Marcin", "Ania", "Agnieszka"];
+console.log(tab5[0] + "!");
+console.log(tab5[1] + "!?");
+const name = tab5[1];
+console.log(name.toUpperCase());
+console.log(tab5[tab5.length-1]);
+
+//Dodawanie elementów do tablicy
+const tab6 = ["Marcin", "Ania", "Agnieszka"];
+tab6.push("Natalia");
+//lub
+tab6[tab6.length] = "Kasia";
+tab6[6] = "Piotrek";//6sty element, 5ty element jest pusty
+console.log(tab6);
+*/
+const tab7 = [];
+tab7[0] = ["Marcin", 183];
+tab7[1] = ["Ania", 173];
+tab7[2] = ["Agnieszka", 170];
+
+console.log('Imie: ');
